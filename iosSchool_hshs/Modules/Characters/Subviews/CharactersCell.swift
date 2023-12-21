d9964043fde29ff75d7a7f1f83f50f81fe023acc
Владислав Береснев <vladislaw.beresnev2018@yandex.ru>
2023-12-21 15:56:21 +0700

    Поправил плейсхолдер для персонажей

--- iosSchool_hshs/Modules/Characters/Subviews/CharactersCell.swift
+++ iosSchool_hshs/Modules/Characters/Subviews/CharactersCell.swift
@@ -11,7 +11,6 @@ class CharactersCell: UICollectionViewCell, CoreCellView {
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var genderSpeciesLabel: UILabel!
-    @IBOutlet private weak var placeholderImageView: UIImageView!
     @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
 
     override func awakeFromNib() {
@@ -28,17 +27,6 @@ class CharactersCell: UICollectionViewCell, CoreCellView {
         layer.shadowOpacity = 1
         layer.shadowRadius = 8
         layer.shadowOffset = CGSize(width: 0, height: 5)
-    }
-
-    private func isPlaceholders(switcher: Bool) {
-        guard switcher else {
-            placeholderImageView.isHidden = true
-            activityIndicator.stopAnimating()
-            return
-        }
-        placeholderImageView.isHidden = false
-        activityIndicator.hidesWhenStopped = switcher
-        activityIndicator.startAnimating()
     }
 
     static func layoutSection() -> NSCollectionLayoutSection {
@@ -60,20 +48,19 @@ class CharactersCell: UICollectionViewCell, CoreCellView {
         group.interItemSpacing = .fixed(24)
         let section = NSCollectionLayoutSection(group: group)
         section.interGroupSpacing = 30
-        section.contentInsets = NSDirectionalEdgeInsets(top: 60, leading: 16, bottom: 70, trailing: 16)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 59, leading: 16, bottom: 0, trailing: 16)
         return section
     }
-
     func update(with inputData: CharactersCellData) {
-        if inputData.isLoading == true {
-            isPlaceholders(switcher: true)
-            nameLabel.text = inputData.name
-            genderSpeciesLabel.text = inputData.genderSpecies
+        nameLabel.text = inputData.name
+        genderSpeciesLabel.text = inputData.genderSpecies
+        if inputData.isLoading {
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.startAnimating()
+            imageView.image = UIImage(named: "characters-placeholder")
         } else {
-            isPlaceholders(switcher: false)
+            activityIndicator.stopAnimating()
             imageView.image = inputData.image
-            nameLabel.text = inputData.name
-            genderSpeciesLabel.text = inputData.genderSpecies
         }
     }
 }
